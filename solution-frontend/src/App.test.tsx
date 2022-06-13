fd431855e8d248cb1ac2b3d8998fe8b134e6912e
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-06-13 22:02:27 +0200

    Test added

--- solution-frontend/src/App.test.tsx
+++ solution-frontend/src/App.test.tsx
@@ -1,10 +1,12 @@
-import React from "react"
-import { screen } from "@testing-library/react"
-import { render } from "./test-utils"
-import { App } from "./App"
+import React from "react";
+import { screen } from "@testing-library/react";
+import { render } from "./test-utils";
+import { App } from "./App";
 
-test("renders learn react link", () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn chakra/i)
-  expect(linkElement).toBeInTheDocument()
-})
+test("renders API key", () => {
+  render(<App />);
+
+  expect(screen.getByTestId("apiKey").textContent).toBe(
+    process.env.REACT_APP_API_KEY
+  );
+});
--- solution-frontend/src/App.tsx
+++ solution-frontend/src/App.tsx
@@ -4,5 +4,6 @@ import * as React from "react";
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Heading>Hello World!</Heading>
+    <Heading data-testid="apiKey">{process.env.REACT_APP_API_KEY}</Heading>
   </ChakraProvider>
 );
